5447762e185da364681da80184edb99c4b4515cd
ACEU901 <andrea.espinozau@correoaiep.cl>
2025-11-21 14:52:15 -0300

    restaurando archivos

--- 3_Entradas/3_1_entrada.py
+++
@@ -1,8 +0,0 @@
-nombre:str = input("Cual es tu nombre?")
-fruta:str = input("Cual es tu fruta favorita?")
-
-print(nombre)
-print(fruta)
-
-print("Hola " + nombre + " tu fruta favorita es la " + fruta)
-
