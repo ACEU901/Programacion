5c51690314c94407c35af3f0117e9d28b2c85573
ACEU901 <andrea.espinozau@correoaiep.cl>
2025-11-21 13:55:45 -0300

    carga de programacion

---
+++ 3_Entradas/3_1_entrada.py
@@ -0,0 +1,8 @@
+nombre:str = input("Cual es tu nombre?")
+fruta:str = input("Cual es tu fruta favorita?")
+
+print(nombre)
+print(fruta)
+
+print("Hola " + nombre + " tu fruta favorita es la " + fruta)
+
---
+++ 4_Variables/4_1_Integer.py
@@ -0,0 +1,16 @@
+"""
+Los integeres (enteros) son numeros sin parte decimal, 
+tanto positivos como negativos, incluyendo el cero.
+"""
+
+#Ejemplo de int
+""" 
+Nombre de la variable, un igual y el valor integer
+"""
+edad = 25
+year = 2025
+temperatura = -10
+
+print("La edad es: ",edad)
+print ("El año es: ",year)
+print("La temperatura es: ",temperatura)
