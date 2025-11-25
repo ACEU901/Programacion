5c0b90e3bf84166a3426d07b0ead94f471fc6abc
ACEU901 <andrea.espinozau@correoaiep.cl>
2025-11-25 12:35:04 -0300

    subimos la carpeta decisiones

---
+++ 8_Desiciones/8_2_Ciclo_if2.py
@@ -0,0 +1,11 @@
+mascota = input("Que mascota tienes? ")
+
+if mascota == "gato":
+    print("Es un gato")
+elif mascota == "perro":
+    print("Es un perro")
+elif mascota == "gallina":
+    print("Es una gallina")
+else:
+    print("Es un unicornio")
+    
